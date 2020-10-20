ee0bd12e129998d9c62ba40206b531bead214b5f
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:10:28 +0800

    first commit

---
+++ src/PBOIF2_10116405_Latihan1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ 
+ */
+package PBOIF2_10116405_LATIHAN1;
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Nama  : Fahrih Fatahilah
+ * Kelas : IF-2
+ * Nim   : 10116405
+ * 
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+      System.out.println("NAMA\t\t: Fahrih Fatahilah");
+      System.out.println("ALAMAT\t\t: Jalan Tubagus Ismail Dalam No.31");
+      System.out.println("TEMPAT LAHIR\t: Bima, Nusa Tenggara Barat");
+      System.out.println("TANGGAL LAHIR\t: 2 Februari 1998");
+      System.out.println("--------------------------");
+      System.out.println("HOBI\t\t: Bikin Music ");
+      System.out.println("MAKES\t\t: Ayam Geprek");
+      System.out.println("MIKES\t\t: Es Kopi Susu");
+    }
+    
+}
